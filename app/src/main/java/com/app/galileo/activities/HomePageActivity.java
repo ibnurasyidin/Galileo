af65421e8374c4718cc06826627888cde05640b2
mrifqiaufaabdika <Rifqi1996>
2020-09-07 09:34:38 +0700

    scegli i tuoi piani

--- app/src/main/java/com/app/galileo/activities/HomePageActivity.java
+++ app/src/main/java/com/app/galileo/activities/HomePageActivity.java
@@ -1,6 +1,9 @@
 package com.app.galileo.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,7 @@ import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -25,6 +29,8 @@ public class HomePageActivity extends AppCompatActivity {
 
     @BindView(R.id.recylerview)
     RecyclerView recylerview;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,5 +49,10 @@ public class HomePageActivity extends AppCompatActivity {
         recylerview.setAdapter(esploraAdapter);
 
 
+    }
+
+    @OnClick(R.id.btn_piani) void piani(){
+        Intent intent = new Intent(this,ScegliITuoiPianiActivity.class);
+        startActivity(intent);
     }
 }
