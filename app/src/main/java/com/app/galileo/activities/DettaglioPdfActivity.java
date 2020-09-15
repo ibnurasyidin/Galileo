62ea60c6bf0d681f5853acbe435898afceec057a
trimuerisandes <trimuerisandes@gmail.com>
2020-09-15 14:48:19 +0700

    pdf detaglio

---
+++ app/src/main/java/com/app/galileo/activities/DettaglioPdfActivity.java
@@ -0,0 +1,16 @@
+package com.app.galileo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.galileo.R;
+
+public class DettaglioPdfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dettaglio_pdf);
+    }
+}
